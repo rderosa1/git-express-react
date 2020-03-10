b7fa63f2ac3b6b961ff081af9bfbec7259d0daf5
ro <r.cosenza39@gmail.com>
2020-03-10 10:09:31 -0400

    blueberry tute

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,15 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>How To Become a Blueberry</h1>
-      <h2>Step 1, Become a Blueberry</h2>
-      <h3>Question Life</h3>
-      <h4>Sleep</h4>
-      <h5>Bananas</h5>
-      <p>some junk in here from mary</p>
-    </div>
+        <h1>How To Become a Blueberry</h1>
+        <h2>Step 1, Become a Blueberry</h2>
+        <h3>Question Life</h3>
+        <h3>Add some blue</h3>
+        <h3>Eat some berries, and remember, it's not exactly cannibalism just yet </h3>
+        <h4>Sleep</h4>
+        <h5>Bananas</h5>
+        <p>some junk in here from mary</p>
+      </div>
     )
   }
 }
