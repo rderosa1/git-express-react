60017a73c90dec5c64c6c4fb3843cbad0527efe4
Jonathanh7 <Jonathanh949@gmail.com>
2020-03-10 09:41:23 -0400

    Who am I

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>How To Become a Blueberry</h1>
+      <h2>Step 1, Become a Blueberry</h2>
+      <h3>Question Life</h3>
+      <h4>Sleep</h4>
+      <h5>Bananas</h5>
     </div>
   );
 }
